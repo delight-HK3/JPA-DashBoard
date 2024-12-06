4975becf9f9950487609a2a323fc713c42648c6e
leebean <dabin310153@naver.com>
2024-12-06 15:33:04 +0900

    파일추가
    파일추가

---
+++ src/main/java/com/spring/jpatest/entity/roomservice.java
@@ -0,0 +1,42 @@
+package com.spring.jpatest.entity;
+
+import org.hibernate.annotations.Comment;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Entity
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class roomservice {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SEQ")
+    @Comment(value = "일련번호")
+    private int seq;   
+
+    @Column(name = "food")
+    @Comment(value = "주문음식")
+    private String food;
+
+    @Column(name = "drink")
+    @Comment(value = "주문드링크")
+    private String drink;
+
+    @Comment(value = "멤버번호")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberid")
+    private Member member;
+}
