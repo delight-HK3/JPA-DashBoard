b63ede188d7c9f7b553f8c0f2128a12aed19a900
leebean <dabin310153@naver.com>
2025-01-10 11:57:44 +0900

    test code add

--- src/test/java/com/spring/jpatest/JpatestApplicationTests.java
+++ src/test/java/com/spring/jpatest/JpatestApplicationTests.java
@@ -4,13 +4,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.spring.jpatest.repository.querydslRepository;
+import com.spring.jpatest.repository.querydslRepositoryImpl;
 import com.spring.jpatest.service.singletonServicetest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JpatestApplicationTests {
+
+	@Autowired
+	private querydslRepository querydslrepository;
 
 	@Test
 	@DisplayName("스프링 없는 순수한 DI 컨테이너")
@@ -27,4 +33,9 @@ public class JpatestApplicationTests {
         singletonServicetest_2.call();
 	}
 
+	@Test
+	@DisplayName("호텔로직이 성공적으로 입력되면 성공")
+	public void entityManagerAdd(){
+		//querydslrepository.insertRoom();
+	}
 }
