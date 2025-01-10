fa2463d4641a4b103c1caabdf008cdf22ce98ac0
leebean <dabin310153@naver.com>
2025-01-10 11:57:16 +0900

    entity colume property change

--- src/main/java/com/spring/jpatest/entity/roomservice.java
+++ src/main/java/com/spring/jpatest/entity/roomservice.java
@@ -1,16 +1,21 @@
 package com.spring.jpatest.entity;
 
+import java.util.List;
+
 import org.hibernate.annotations.Comment;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -19,7 +24,7 @@ import lombok.ToString;
 @Entity
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class roomservice {
+public class Roomservice {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,16 +32,28 @@ public class roomservice {
     @Comment(value = "일련번호")
     private int seq;   
 
-    @Column(name = "food")
+    @Column(name = "food", length = 50)
     @Comment(value = "주문음식")
     private String food;
 
-    @Column(name = "drink")
+    @Column(name = "drink", length = 50)
     @Comment(value = "주문드링크")
     private String drink;
 
-    @Comment(value = "멤버번호")
+    @Comment(value = "숙소번호")
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "room_seq")
+    private Room room;
+    
+    @Comment(value = "투숙객번호")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_seq")
     private Member member;
+
+    public Roomservice(String food, String drink, Room room, Member member){
+        this.food = food;
+        this.drink = drink;
+        this.room = room;
+        this.member = member;
+    }
 }
