456a66309426010eea5c855a51c2db87310ea3fe
leebean <dabin310153@naver.com>
2025-01-13 16:32:35 +0900

    entity cache add

--- src/main/java/com/spring/jpatest/entity/springjpaMainEntity.java
+++ src/main/java/com/spring/jpatest/entity/springjpaMainEntity.java
@@ -1,5 +1,7 @@
 package com.spring.jpatest.entity;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.Comment;
 
 import jakarta.persistence.Column;
@@ -11,10 +13,12 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AccessLevel;
+import lombok.Builder;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 @Table(name="t_springdatajpa_main")
 public class springjpaMainEntity {
 
@@ -35,4 +39,11 @@ public class springjpaMainEntity {
     @Column(name = "UNIQUENUM")
     @Comment(value = "고유번호")
     private int uniqueNum;
+
+    @Builder
+    public springjpaMainEntity(String name, int age, int uniqueNum){
+        this.age = age;
+        this.name = name;
+        this.uniqueNum = uniqueNum;
+    }
 }
