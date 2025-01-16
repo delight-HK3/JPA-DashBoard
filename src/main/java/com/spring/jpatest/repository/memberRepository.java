2cde1f6b5a0c06fca4bddda6c67b2d39f071c645
leebean <dabin310153@naver.com>
2025-01-16 14:09:25 +0900

    file delete

--- src/main/java/com/spring/jpatest/repository/memberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.jpatest.repository;
-
-
-import com.spring.jpatest.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface memberRepository extends JpaRepository<Member, Integer>{
-    
-}
