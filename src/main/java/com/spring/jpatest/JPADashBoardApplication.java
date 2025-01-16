985d2744d3a6fe639e9259efd7f2266fcfbe5278
leebean <dabin310153@naver.com>
2025-01-16 14:16:54 +0900

    main class name change

--- src/main/java/com/spring/jpatest/JPADashBoardApplication.java
+++ src/main/java/com/spring/jpatest/JPADashBoardApplication.java
@@ -2,12 +2,11 @@ package com.spring.jpatest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class JpatestApplication {
+public class JPADashBoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpatestApplication.class, args);
+		SpringApplication.run(JPADashBoardApplication.class, args);
 	}
 
 }
