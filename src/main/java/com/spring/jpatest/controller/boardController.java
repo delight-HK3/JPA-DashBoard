77250afd982584ef47787ceb095a34d542a79d86
leebean <dabin310153@naver.com>
2025-01-21 12:42:32 +0900

    게시판 정보 가져오는 코드 추가

--- src/main/java/com/spring/jpatest/controller/boardController.java
+++ src/main/java/com/spring/jpatest/controller/boardController.java
@@ -1,12 +1,24 @@
 package com.spring.jpatest.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.jpatest.dto.boardDTO;
+import com.spring.jpatest.service.boardService;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class boardController {
+
+    private final boardService boardservice;
+
+    public boardController(boardService boardservice){
+        this.boardservice = boardservice;
+    }
 
     /**
      * 게시판 로딩
@@ -17,9 +29,9 @@ public class boardController {
     @RequestMapping(value = "/", method=RequestMethod.GET)
     public ModelAndView requestMethodName(ModelAndView mav) {
 
-        
+        List<boardDTO> boardList = boardservice.getBoardList();
 
-
+        mav.addObject("boardList", boardList);
         mav.setViewName("content/index");
 
         return mav;
