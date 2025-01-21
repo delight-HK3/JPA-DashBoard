9aa8f1b5d18315052caf66c0cafa84eb0ee7b36f
leebean <dabin310153@naver.com>
2025-01-21 12:42:09 +0900

    file name change

--- src/main/java/com/spring/jpatest/config/SecurityConfig.java
+++ src/main/java/com/spring/jpatest/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfig {
     
     @Bean
 	public PasswordEncoder getPasswordEncoder() {
