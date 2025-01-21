a2a30f4fe40b11bf60d6769aa18839325e57e5c1
leebean <dabin310153@naver.com>
2025-01-21 12:46:29 +0900

    field argument change

--- src/main/java/com/spring/jpatest/dto/boardDTO.java
+++ src/main/java/com/spring/jpatest/dto/boardDTO.java
@@ -2,23 +2,25 @@ package com.spring.jpatest.dto;
 
 import java.time.LocalDate;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class boardDTO {
     
-    private String boardTitle;
-    private int viewCnt; 
-    private int likeCnt;
-    private LocalDate instDate;
+    private final int boardNum;
+    private final String boardTitle;
+    private final int viewCnt; 
+    private final int likeCnt;
+    private final LocalDate instDate;
+    private final String nickName;
 
-    @Builder
-    public boardDTO(String boardTitle, int viewCnt,
-                    int likeCnt, LocalDate instDate){
+    public boardDTO(int boardNum, String boardTitle, int viewCnt,
+                    int likeCnt, LocalDate instDate, String nickName){
+        this.boardNum = boardNum;
         this.boardTitle = boardTitle;
         this.viewCnt = viewCnt;
         this.likeCnt = likeCnt;
         this.instDate = instDate;
+        this.nickName = nickName;
     } 
 }
