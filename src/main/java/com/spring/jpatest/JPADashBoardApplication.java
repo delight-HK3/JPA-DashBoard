875bec5ba8348ebd680be4c56194be94f8d26485
leebean <dabin310153@naver.com>
2025-01-22 13:17:36 +0900

    Update JPADashBoardApplication.java

--- src/main/java/com/spring/jpatest/JPADashBoardApplication.java
+++ src/main/java/com/spring/jpatest/JPADashBoardApplication.java
@@ -2,6 +2,7 @@ package com.spring.jpatest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class JPADashBoardApplication {
 
