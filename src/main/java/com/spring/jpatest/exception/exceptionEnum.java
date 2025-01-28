ed8231faa542ff422e69dada554850b17120c438
leebean <dabin310153@naver.com>
2025-01-28 13:37:04 +0900

    message add

--- src/main/java/com/spring/jpatest/exception/exceptionEnum.java
+++ src/main/java/com/spring/jpatest/exception/exceptionEnum.java
@@ -10,7 +10,13 @@ import lombok.ToString;
 public enum exceptionEnum {
     
     // 유저등록시 값이 없는 경우
-    USER_INSERT_DATA_NULL(HttpStatus.NOT_FOUND,"유저 등록에 필요한 정보가 부족합니다.");
+    USER_INSERT_DATA_NULL(HttpStatus.NOT_FOUND,"유저 등록에 필요한 정보가 부족합니다."),
+    
+    // 유저정보가 없는 경우
+    NO_USER_INFO(HttpStatus.NOT_FOUND, "유저정보가 없습니다."),
+
+    // 유저 정보는 있으나 비밀번호가 일치하지 않는 경우
+    NOT_MATCH_PASS(HttpStatus.NOT_FOUND,"비밀번호가 올바르지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
