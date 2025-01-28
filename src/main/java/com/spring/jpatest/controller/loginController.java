4b8687aab74b01d1bc05ecfa5bff012edd26f594
leebean <dabin310153@naver.com>
2025-01-28 13:35:06 +0900

    login add

--- src/main/java/com/spring/jpatest/controller/loginController.java
+++ src/main/java/com/spring/jpatest/controller/loginController.java
@@ -3,22 +3,41 @@ package com.spring.jpatest.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.spring.jpatest.dto.login.loginDTO;
+import com.spring.jpatest.dto.login.loginResponseDTO;
+import com.spring.jpatest.service.loginService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
 public class loginController {
-    
-    /**
-     * 회원로그인
-     * 
-     * @param param
-     */
+
+    private final loginService loginService;
+
+    public loginController(loginService loginService){
+        this.loginService = loginService;
+    }
+
+     /**
+      * 회원로그인 
+      *
+      * @param logindto
+      * @param request
+      */
     @ResponseBody
     @RequestMapping(value="/login", method=RequestMethod.GET)
-    public void requestMethodName(@RequestParam String param) {
+    public String userlogin(loginDTO logindto, HttpServletRequest request) {
         
+        HttpSession session = request.getSession(true); 
+        loginResponseDTO result = loginService.getUserInfo(logindto);
+
+        session.setAttribute("nickName",result.getNickName());
+        
+        return "redirect:/";
     }
     
 
