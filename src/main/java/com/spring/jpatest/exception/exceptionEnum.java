ef243a01d21fd1ed721a2fddb6c11cb2424278fb
leebean <dabin310153@naver.com>
2025-01-29 17:22:09 +0900

    exception enum change

--- src/main/java/com/spring/jpatest/exception/exceptionEnum.java
+++ src/main/java/com/spring/jpatest/exception/exceptionEnum.java
@@ -12,11 +12,8 @@ public enum exceptionEnum {
     // 유저등록시 값이 없는 경우
     USER_INSERT_DATA_NULL(HttpStatus.NOT_FOUND,"유저 등록에 필요한 정보가 부족합니다."),
     
-    // 유저정보가 없는 경우
-    NO_USER_INFO(HttpStatus.NOT_FOUND, "유저정보가 없습니다."),
-
-    // 유저 정보는 있으나 비밀번호가 일치하지 않는 경우
-    NOT_MATCH_PASS(HttpStatus.NOT_FOUND,"비밀번호가 올바르지 않습니다.");
+    // 아이디 혹은 비밀번호가 일치하지 않은 경우우
+    NOT_MATCH_INFO(HttpStatus.NOT_FOUND, "아이디 혹은 비밀번호가 올바르지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/spring/jpatest/exception/exceptionHandler.java
+++ src/main/java/com/spring/jpatest/exception/exceptionHandler.java
@@ -1,11 +1,17 @@
 package com.spring.jpatest.exception;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.spring.jpatest.exception.custom.NoMatchInfoException;
+
+import jakarta.servlet.ServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -15,6 +21,7 @@ public class exceptionHandler {
     // 유저등록시 값이 없는 경우
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ResponseEntity<?> NullPointerException(final NullPointerException e){
+        
         String message = getExceptionMessage(e.getMessage());
         StackTraceElement[] stackTraceElement = e.getStackTrace();
         log.error(message, stackTraceElement[0]);
@@ -23,6 +30,21 @@ public class exceptionHandler {
                 .status(exceptionEnum.USER_INSERT_DATA_NULL.getStatus())
                 .body(exceptionEnum.USER_INSERT_DATA_NULL.getMessage());
     } 
+
+    // 아이디 혹은 비밀번호가 일치하지 않는 경우
+    @ExceptionHandler({NoMatchInfoException.class})
+    public ResponseEntity<?> NoMatchInfoException(final RuntimeException e) {
+
+        String message = getExceptionMessage(e.getMessage());
+        StackTraceElement[] stackTraceElement = e.getStackTrace();
+        log.error(message, stackTraceElement[0]);
+        
+        return ResponseEntity
+                .status(exceptionEnum.NOT_MATCH_INFO.getStatus())
+                .body(exceptionEnum.NOT_MATCH_INFO.getMessage());
+
+    }
+
 
     private String getExceptionMessage(String message){
         if(StringUtils.hasText(message)){
