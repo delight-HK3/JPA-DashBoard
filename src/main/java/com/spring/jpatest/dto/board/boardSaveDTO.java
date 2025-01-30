b1d1ff5ecb1fcef11996fb75ca84f3449f1d5914
leebean <dabin310153@naver.com>
2025-01-30 13:19:35 +0900

    Create boardSaveDTO.java

---
+++ src/main/java/com/spring/jpatest/dto/board/boardSaveDTO.java
@@ -0,0 +1,19 @@
+package com.spring.jpatest.dto.board;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class boardSaveDTO {
+    
+    private UUID useruuid;          // 유저 일련번호
+    private int boardSeq;           // 게시글 일련번호
+    private LocalDate instDate;        // 게시글 등록일
+    private String boardTitle;      // 게시글 제목
+    private String boardSubject;    // 게시글 내용
+
+}
