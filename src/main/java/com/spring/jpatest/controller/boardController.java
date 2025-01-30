f6c1ab69ebbc78470065f6b0c9862bde093fda71
leebean <dabin310153@naver.com>
2025-01-30 13:19:40 +0900

    Update boardController.java

--- src/main/java/com/spring/jpatest/controller/boardController.java
+++ src/main/java/com/spring/jpatest/controller/boardController.java
@@ -1,13 +1,19 @@
 package com.spring.jpatest.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.spring.jpatest.dto.board.boardDTO;
+import com.spring.jpatest.dto.board.boardListDTO;
+import com.spring.jpatest.dto.board.boardSaveDTO;
 import com.spring.jpatest.service.boardService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,22 +27,45 @@ public class boardController {
     }
 
     /**
-     * 게시판 로딩
+     * board - 메인 게시판 로딩
      * 
      * @param mav
      * @return
      */
     @RequestMapping(value = "/", method=RequestMethod.GET)
-    public ModelAndView requestMethodName(ModelAndView mav) {
+    public ModelAndView boardMain(ModelAndView mav) {
 
-        List<boardDTO> boardList = boardservice.getBoardList();
+        List<boardListDTO> boardList = boardservice.getBoardList();
 
         mav.addObject("boardList", boardList);
-        mav.setViewName("content/index");
+        mav.setViewName("board/boardListPage");
 
         return mav;
     }
 
+    /**
+     * board - 게시글 입력 페이지 로딩
+     * 
+     * @param mav
+     * @return
+     */
+    @RequestMapping(value="/board/add", method=RequestMethod.GET)
+    public ModelAndView boardAddPage(boardSaveDTO boardSavedto, ModelAndView mav) {
 
+        mav.addObject("boarddto", boardSavedto);
+        mav.setViewName("board/boardAddPage");
 
+        return mav;
+    }
+
+    @RequestMapping(value="/board/save", method=RequestMethod.POST)
+    public String boardSave(@ModelAttribute("boarddto") boardSaveDTO boardSavedto, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        boardSavedto.setUseruuid((UUID) session.getAttribute("useruuid"));
+        
+        boardservice.boardSave(boardSavedto);
+
+        return "redirect:/";
+    }
 }
