02b1086e8c1f28d73ea5fa3e0b35b43373450eec
leebean <dabin310153@naver.com>
2025-01-31 17:25:55 +0900

    handler edit
    - If the post does not exist handler add

--- src/main/java/com/spring/jpatest/exception/exceptionHandler.java
+++ src/main/java/com/spring/jpatest/exception/exceptionHandler.java
@@ -1,8 +1,7 @@
 package com.spring.jpatest.exception;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,7 +10,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.spring.jpatest.exception.custom.NoMatchInfoException;
 
-import jakarta.servlet.ServletResponse;
+import jakarta.persistence.NoResultException;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -20,7 +20,7 @@ public class exceptionHandler {
     
     // 유저등록시 값이 없는 경우
     @ExceptionHandler({MethodArgumentNotValidException.class})
-    public ResponseEntity<?> NullPointerException(final NullPointerException e){
+    public ResponseEntity<?> MethodArgumentNotValidExceptionHandler(final NullPointerException e){
         
         String message = getExceptionMessage(e.getMessage());
         StackTraceElement[] stackTraceElement = e.getStackTrace();
@@ -33,7 +33,7 @@ public class exceptionHandler {
 
     // 아이디 혹은 비밀번호가 일치하지 않는 경우
     @ExceptionHandler({NoMatchInfoException.class})
-    public ResponseEntity<?> NoMatchInfoException(final RuntimeException e) {
+    public ResponseEntity<?> NoMatchInfoExceptionHandler(final RuntimeException e) {
 
         String message = getExceptionMessage(e.getMessage());
         StackTraceElement[] stackTraceElement = e.getStackTrace();
@@ -42,9 +42,23 @@ public class exceptionHandler {
         return ResponseEntity
                 .status(exceptionEnum.NOT_MATCH_INFO.getStatus())
                 .body(exceptionEnum.NOT_MATCH_INFO.getMessage());
-
     }
 
+    // 게시글이 존재하지 않는 경우
+    // NullPointerException : (Exception 조건 : 해당 게시글이 존재하지 않는 경우)
+    @ExceptionHandler({NullPointerException.class})
+    public ResponseEntity<String> NoDataExceptionHandler(final RuntimeException e) {
+
+        String message = getExceptionMessage(e.getMessage());
+        StackTraceElement[] stackTraceElement = e.getStackTrace();
+        log.error(message, stackTraceElement[0]);
+        
+        String script = "<script>alert('해당 게시글이 존재하지 않습니다.'); location.href='/'</script>";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "text/html; charset=UTF-8");
+
+        return new ResponseEntity<>(script, headers, HttpStatus.OK);
+    }
 
     private String getExceptionMessage(String message){
         if(StringUtils.hasText(message)){
