dd34a31503b18a0feeea160ec8b6d9ad8d29ba33
leebean <dabin310153@naver.com>
2025-01-31 17:24:47 +0900

    Update boardSaveDTO.java

--- src/main/java/com/spring/jpatest/dto/board/boardSaveDTO.java
+++ src/main/java/com/spring/jpatest/dto/board/boardSaveDTO.java
@@ -12,7 +12,7 @@ public class boardSaveDTO {
     
     private UUID useruuid;          // 유저 일련번호
     private int boardSeq;           // 게시글 일련번호
-    private LocalDate instDate;        // 게시글 등록일
+    private LocalDate instDate;     // 게시글 등록일
     private String boardTitle;      // 게시글 제목
     private String boardSubject;    // 게시글 내용
 
