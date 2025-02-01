bb3c357cd4934cd4fb1892518a154aac25fbf35b
leebean <dabin310153@naver.com>
2025-02-01 14:08:44 +0900

    method add
    - getBoardDetail method add
    - boardCntUp method add

--- src/main/java/com/spring/jpatest/repository/boardRepository.java
+++ src/main/java/com/spring/jpatest/repository/boardRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.spring.jpatest.dto.board.boardDetailDTO;
 import com.spring.jpatest.dto.board.boardListDTO;
 import com.spring.jpatest.dto.board.boardSaveDTO;
 
@@ -12,6 +13,10 @@ public interface boardRepository{
 
     public List<boardListDTO> getBoardList(); // 리스트 조회
     
+    public boardDetailDTO getBoardDetail(int boardCd); // 게시글 상세정보 호출
+
+    public void boardCntUp(int boardCd); // 게시글 조회수 1 증가
+
     public void boardSave(boardSaveDTO boardSavedto); // 등록및 저장
 
     public void boardDelete(); // 게시글 삭제
