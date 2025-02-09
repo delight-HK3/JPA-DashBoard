3fe8009e381a6f54ed39b734ca803f24d7f1fdaa
leebean <dabin310153@naver.com>
2025-02-09 16:50:15 +0900

    boardEdit testcode add

--- src/test/java/com/spring/jpatest/boardTechTest.java
+++ src/test/java/com/spring/jpatest/boardTechTest.java
@@ -1,11 +1,14 @@
 package com.spring.jpatest;
 
+import static com.spring.jpatest.domain.QBoard.board;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.spring.jpatest.dto.board.boardDetailDTO;
+import com.spring.jpatest.dto.board.boardSaveDTO;
 import com.spring.jpatest.service.boardService;
 
 @SpringBootTest
@@ -35,4 +38,31 @@ public class boardTechTest {
 
 	}
 
+	@Test
+	@DisplayName("특정 게시글 업데이트 (성공)")
+	public void updateboardSuccess(){
+		
+		boardSaveDTO boardDto = new boardSaveDTO();
+
+		boardDto.setBoardSeq(2);
+		boardDto.setBoardTitle("1234sadfasdfdsa");
+		boardDto.setBoardSubject("1234aegsdfgsdfgsdfg");
+
+		boardService.boardSave(boardDto);
+
+	}
+
+	@Test
+	@DisplayName("특정 게시글 업데이트 (실패)")
+	public void updateboardFailed(){
+		
+		boardSaveDTO boardDto = new boardSaveDTO();
+
+		boardDto.setBoardSeq(29);
+		boardDto.setBoardTitle("1234sadfasdfdsa");
+		boardDto.setBoardSubject("1234aegsdfgsdfgsdfg");
+
+		boardService.boardSave(boardDto);
+
+	}
 }
