10f36987c7fc967e793592280c2e45ba560269a9
leebean <dabin310153@naver.com>
2025-02-11 17:20:41 +0900

    extends add
    - boardRepositoryCustom add

--- src/main/java/com/spring/jpatest/repository/board/boardRepository.java
+++ src/main/java/com/spring/jpatest/repository/board/boardRepository.java
@@ -1,15 +1,11 @@
 package com.spring.jpatest.repository.board;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.spring.jpatest.domain.Board;
-import com.spring.jpatest.domain.User;
-
 
 @Repository
-public interface boardRepository extends JpaRepository<Board, Integer>{
-    Optional<Board> findByUserBoard(User user, Board board);
+public interface boardRepository extends JpaRepository<Board, Integer>, boardRepositoryCustom{
+
 } 
