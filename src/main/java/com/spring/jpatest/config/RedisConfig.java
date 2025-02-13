2a6c6ecf4025caef0f480c714121fbe592d1de60
leebean <dabin310153@naver.com>
2025-02-13 16:43:37 +0900

    Redis config add

---
+++ src/main/java/com/spring/jpatest/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.spring.jpatest.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+    
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // Jedis와 Lettuce중에 Lettuce를 사용했는데 Jedis는 멀티 쓰레드가 불안정하고 Pool의 한계가 있고
+        // Lettuce는 Netty 기반이라 비동기 지원 가능하여 Lettuce를 사용했다.
+        return new LettuceConnectionFactory(host, port);
+    }
+}
