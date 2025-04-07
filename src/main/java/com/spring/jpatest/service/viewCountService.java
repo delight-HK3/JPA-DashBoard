909d7c860c094be07296998ce8eec16219113e3f
leedabean <dabin310153@naver.com>
2025-04-07 18:07:18 +0900

    redisconfig delete

--- src/main/java/com/spring/jpatest/service/viewCountService.java
+++ src/main/java/com/spring/jpatest/service/viewCountService.java
@@ -5,23 +5,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class viewCountService {
-    
+    /* 
     private final RedisTemplate<String, Integer> redisTemplate;
 
     public viewCountService(RedisTemplate<String, Integer> redisTemplate){
         this.redisTemplate = redisTemplate;
-    }
+    }*/
 
     // 특정 게시글이 조회되었는지 확인(존재하면 true, 존재하지 않으면 false)
     // SetOperations는 Redis에서 집합을 조작하기 위한 클래스
     public boolean isViewed(int boardCd) {
         String key = "boardCd" + boardCd;
-        return redisTemplate.opsForSet().isMember(key,boardCd);
+        return false; //redisTemplate.opsForSet().isMember(key,boardCd);
     }
 
     public void addView(int boardCd) {
         String key = "boardCd" + boardCd;
-        redisTemplate.opsForSet().add(key, boardCd);
+        //redisTemplate.opsForSet().add(key, boardCd);
     }
 
     // isViewed를 호출하여 이미 해당 페이지 조회 여부 확인후 Redis에 게시글 등록
@@ -32,12 +32,12 @@ public class viewCountService {
     public void increaseViewCount(int boardCd, int viewCnt) {
         String key = "viewCnt" + boardCd;
         addView(boardCd);
-        redisTemplate.opsForValue().set(key, viewCnt + 1);
+        //redisTemplate.opsForValue().set(key, viewCnt + 1);
     }
 
     // 해당 게시글의 조회수 조회
     public int getViewCount(int boardCd) {
         String key = "viewCnt" + boardCd;
-        return redisTemplate.opsForValue().get(key);
+        return 1; //redisTemplate.opsForValue().get(key);
     }
 }
