71561d259e10332647636221e844e540becc2e62
leebean <dabin310153@naver.com>
2025-04-08 14:52:55 +0900

    getter add

--- src/main/java/com/spring/jpatest/exception/custom/NoBoardDataException.java
+++ src/main/java/com/spring/jpatest/exception/custom/NoBoardDataException.java
@@ -2,6 +2,9 @@ package com.spring.jpatest.exception.custom;
 
 import com.spring.jpatest.exception.exceptionEnum;
 
+import lombok.Getter;
+
+@Getter
 public class NoBoardDataException extends RuntimeException{
     private exceptionEnum error;
 
