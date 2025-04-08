05bf5cca2d5f4ce57c81099fe86e997c6250d3be
leebean <dabin310153@naver.com>
2025-04-08 14:52:50 +0900

    getter add

--- src/main/java/com/spring/jpatest/exception/custom/AlreadyLikeException.java
+++ src/main/java/com/spring/jpatest/exception/custom/AlreadyLikeException.java
@@ -1,7 +1,9 @@
 package com.spring.jpatest.exception.custom;
 
 import com.spring.jpatest.exception.exceptionEnum;
+import lombok.Getter;
 
+@Getter
 public class AlreadyLikeException extends RuntimeException{
    private exceptionEnum error;
 
