768dfa4e163792c59c3978524682e4a2713a994a
leebean <dabin310153@naver.com>
2025-04-09 14:52:19 +0900

    Optional로 wrapping

--- src/main/java/com/spring/jpatest/repository/board/boardRepositoryCustom.java
+++ src/main/java/com/spring/jpatest/repository/board/boardRepositoryCustom.java
@@ -1,12 +1,12 @@
 package com.spring.jpatest.repository.board;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import com.spring.jpatest.domain.Board;
 import com.spring.jpatest.dto.board.boardDetailDTO;
 import com.spring.jpatest.dto.board.boardListDTO;
 import com.spring.jpatest.dto.board.boardSaveDTO;
@@ -15,7 +15,7 @@ public interface boardRepositoryCustom{
 
     public Page<boardListDTO> getBoardList(Pageable pageable); // 리스트 조회
     
-    public boardDetailDTO getBoardDetail(int boardCd); // 게시글 상세정보 호출
+    public Optional<boardDetailDTO> getBoardDetail(int boardCd); // 게시글 상세정보 호출
 
     public void boardCntUp(int boardCd); // 게시글 조회수 1 증가
 
